33c8ebdee7bc9f377e5e9c58ff4be26a5641ab04
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-11-18 17:40:44 +0100

    Add code in main method

--- CommunicationService/Program.cs
+++ CommunicationService/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace Jaroszek.ProofOfConcept.CommunicationService
+﻿using System;
+using System.Globalization;
+using Topshelf;
+
+namespace Jaroszek.ProofOfConcept.CommunicationService
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var host = HostFactory.Run(
+                x =>
+            {
+                //  x.UseSerilog();
+
+
+
+                x.RunAsLocalSystem();
+                x.SetDescription("");
+                x.SetDisplayName("");
+                x.SetServiceName("");
+
+
+            });
+
+            Environment.ExitCode = (int)Convert.ChangeType(host, host.GetTypeCode(), CultureInfo.InvariantCulture);
+
+
+
         }
     }
 }
